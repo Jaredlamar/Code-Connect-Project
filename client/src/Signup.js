97a693049e38715fd9cd63e59c1d6e0b141553ca
Jared <redlaward@gmail.com>
2022-06-12 12:41:24 -0400

    removed text

--- client/src/Signup.js
+++ client/src/Signup.js
@@ -86,7 +86,7 @@ function Signup({ setCurrentUser, currentUser }) {
               <h1>Login</h1>
               <input type="text" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)}/>
               <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)}/>
-              <button type="submit">Login</button>
+              <button type="submit"></button>
             </form>
           </div>
         )}
